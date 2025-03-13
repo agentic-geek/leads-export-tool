264e1b6f6c23ded45184324d93e5a0fd7d75fd5e
agentic-geek <ankur@mattoocg.com>
2025-03-13 15:53:21 -0700

    Update schema API to use relative import paths

--- app/api/schema/route.ts
+++ app/api/schema/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getTableSchema } from '@/app/lib/bigquery';
+import { getTableSchema } from '../../lib/bigquery';
 
 export async function GET() {
   console.log('Schema API called');
